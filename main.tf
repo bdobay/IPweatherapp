17729ce720e6b7cccd11dda8bc17616833f2ac77
bdobay <dobayblake@gmail.com>
2022-04-27 16:55:38 +1000

    integrate Terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Redhat" {
+ami = "ami-03ededff12e34e59e"
+instance_type = "t2.micro"
+key_name = "Devops"
+security_groups = ["default"]
+count = 2
+}
+
+
+
+
+output "instance_ip_addr" {
+value = aws_instance.Redhat[*].public_ip
+
+}
