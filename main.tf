841187b89e0aa7560c92b118e7d2eb3422bbaa0d
bdobay <dobayblake@gmail.com>
2022-05-05 22:31:40 +1000

    Ansible integration

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ count = 2
 
 
 resource "aws_lb_target_group_attachment" "test" {
-  num = aws_instance.Redhat.count
+  num = length(aws_instance.Redhat)
   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:128570722676:targetgroup/testgroup/080953d9e5b2998d"
   target_id        = aws_instance.Redhat[num.index].id
   port             = 5004
