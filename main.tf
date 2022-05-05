ced8e00ac9feb7584f47d13fa51a5fc180d88453
bdobay <dobayblake@gmail.com>
2022-05-05 22:08:44 +1000

    Ansible integration

--- main.tf
+++ main.tf
@@ -10,9 +10,18 @@ security_groups = ["default"]
 count = 2
 }
 
+output "instance_ip_addr" {
+value = aws_instance.Redhat[*].public_ip
+
+}
+
+output "instance_id" {
+value = aws_instance.Redhat[*].id
+}
+
 resource "aws_lb_target_group_attachment" "test" {
   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:128570722676:targetgroup/testgroup/080953d9e5b2998d"
-  target_id        = "aws_instance.Redhat[1]"
+  target_id        = instance_id
   port             = 5004
 }
 
