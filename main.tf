f1c38e4bd361aedd72ecc2845bc26a8f00344d02
bdobay <dobayblake@gmail.com>
2022-05-05 22:49:48 +1000

    Ansible integration

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ ami = "ami-03ededff12e34e59e"
 instance_type = "t2.micro"
 key_name = "AppMachines"
 security_groups = ["default"]
-count = ${var.numInstance}
+count = "${var.numInstance}"
 }
 
 
 
 resource "aws_lb_target_group_attachment" "test" {
-  num = ${var.numInstance} 
+  num = "${var.numInstance}"
   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:128570722676:targetgroup/testgroup/080953d9e5b2998d"
   target_id        = aws_instance.Redhat[num.index].id
   port             = 5004
